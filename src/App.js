2ad5ce384b13c42953cb68ee6b57a899fab62755
aleksandreleskin <eleskin1201@gmail.com>
2020-12-12 14:36:15 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'antd/dist/antd.css';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +22,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
