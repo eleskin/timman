6fb148c03cc661b747d4f933d7a45f3db06023b5
aleksandreleskin <eleskin1201@gmail.com>
2020-12-29 23:10:36 +0300

    commit

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -23,8 +23,8 @@ const Sidebar = props => {
       {
         props.auth
         &&
-        <Menu theme="dark" defaultSelectedKeys={location.pathname} mode="inline">
-          <Menu.Item key="/home" icon={<HomeOutlined/>}  className={styles.sidebar__link}>
+        <Menu theme="dark" selectedKeys={[location.pathname]} mode="inline">
+          <Menu.Item key="/home" icon={<HomeOutlined/>} className={styles.sidebar__link}>
             <Link to="/home">Home</Link>
           </Menu.Item>
           <Menu.Item key="/documents" icon={<FileOutlined/>} className={styles.sidebar__link}>
